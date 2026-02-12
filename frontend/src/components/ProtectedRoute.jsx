046242f913990493e8287656060644173766b80b
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-12 21:31:43 +0200

    protecting routes for products

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
 
-  if (!token) {
-    return <Navigate to="/" />;
-  }
+  if (!token) return <Navigate to="/" />;
+
+  if (role !== "admin") return <Navigate to="/" />;
 
   return children;
 }
