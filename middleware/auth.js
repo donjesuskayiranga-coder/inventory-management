16f541e1e17141e2e3413a73fde3ab7f09f98b44
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-13 19:21:13 +0200

    adding more protection

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
+  const authHeader = req.header("Authorization");
 
-const token = req.header("Authorization")?.replace("Bearer", "");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
 
-if (!token) {
-    return res.status(401).json({message: "No token, authorization denied"});
-}
-
-
-try {
-    const decoded = jwt.verify(token , process.env.JWT_SECRET);
+  const token = authHeader.split(" ")[1]; 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-} catch (error) {
-    res.status(401).json({message: "Token is not valid"});
-
-}
-
+  } catch (error) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
 };
+
 module.exports = auth;
