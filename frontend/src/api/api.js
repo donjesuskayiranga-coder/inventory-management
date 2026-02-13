5bfecfa1a080a649bed7cfc92ca8c555592988cd
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-13 19:18:48 +0200

    add more routes for frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,52 +1,15 @@
+import axios from "axios";
 
-const AUTH_URL = "http://localhost:7000/api/auth";
-const PRODUCT_URL = "http://localhost:7000/api/products";
-export const loginUser = async (email, password) => {
-  const response = await fetch(`${AUTH_URL}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  });
+const API = axios.create({
+  baseURL: "http://localhost:7000/api"
+});
 
-  return response.json();
-};
-
-export const registerUser = async (username, email, password) => {
-  const response = await fetch(`${AUTH_URL}/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, email, password }),
-  });
-
-  return response.json();
-};
-export const getProducts = async () => {
+API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = token;
+  }
+  return req;
+});
 
-  const response = await fetch(PRODUCT_URL, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return response.json();
-};
-
-export const createProduct = async (productData) => {
-  const token = localStorage.getItem("token");
-
-  const response = await fetch(PRODUCT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(productData),
-  });
-
-  return response.json();
-};
+export default API;
