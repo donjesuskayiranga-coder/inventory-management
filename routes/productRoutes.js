ccbd8f03740026a577fb6e1762740505234fb2ae
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-13 19:21:54 +0200

    adding more products by the admin and users

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+
 
 const {
     getProducts,
@@ -15,6 +17,13 @@ router.get("/", auth, getProducts);
 router.get("/:id", auth, getProductById);
 router.post("/", auth, createProduct);
 router.post("/", auth, updateProduct);
+router.post("/", auth, admin, async (req, res) => {
+    try {     const { name, description, price, quantity } = req.body;}
+    catch (error) {
+        res.status(500).json({ message: error.message });
+        
+    }
+})
 router.put("/:id", auth, updateProduct);
 router.delete("/:id", auth, deleteProduct);
 module.exports = router;
