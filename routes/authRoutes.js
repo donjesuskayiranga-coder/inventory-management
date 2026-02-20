361f4693893d4543ad22b699c3bb816908219241
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-20 17:38:34 +0200

    adding more authorization to routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const User = require("../models/user");
 const router = express.Router();
 const { register, login} = require("../controllers/authController");
-
 router.post("/register",register);
 
 router.post("/login",login);
+
 router.get("/protected",auth, (req,res) => {
     res.json({message: `Hello user ${req.user.id}`});
+});
+
+router.get("/users", auth, admin, async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+
 });
 
 module.exports = router;
