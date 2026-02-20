f3509835791a4281434b08065eaad532e00c8a10
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-20 17:20:27 +0200

    removing many admins to remain with one on db

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/inventory-
 
 
 const connectDB = async () => {
+    
     try {
         await mongoose.connect(MONGO_URI);
         console.log("MongoDB connected successfully");
