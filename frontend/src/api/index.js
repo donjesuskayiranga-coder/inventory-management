86688589a79162158a3909a9e13ea908b628322b
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-20 17:41:52 +0200

    adding more connections

---
+++ frontend/src/api/index.js
@@ -0,0 +1,15 @@
+const API_BASE = "http://localhost:7000/api";
+
+export async function apiFetch(path, options = {}) {
+  const token = localStorage.getItem("token");
+  const res = await fetch(`${API_BASE}${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+    ...options,
+  });
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.message || "Request failed");
+  return data;
+}
