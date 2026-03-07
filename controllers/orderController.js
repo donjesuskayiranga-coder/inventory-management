ab34d359a5edde16b33d4b7592320ed364fc2e59
Kayiranga <donjesuskayiranga@gmail.com>
2026-03-07 19:33:25 +0200

    simple changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,14 +4,11 @@ exports.getOrders = async ( req, res) => {
     try {
         const orders = await Order.find({user:req.user.id}).populate("products.product");
         res.json(orders);
-
     } catch(err) {
-
         res.status(500).json({message:err.message});
     }
 };
 exports.getOrderById = async (req, res) => {
-
     try {
         const order = await Order.findById(req.params.id).populate("products.product");
         if(!order ) return res.status(404).json({message:"order not found"});
@@ -23,7 +20,6 @@ exports.getOrderById = async (req, res) => {
     }
 };
 exports.createOrder = async (req , res) => {
-
     try{
         const { products} = req.body;
          const order = await Order.create({
@@ -44,11 +40,7 @@ exports.updateOrder = async (req, res) => {
         const order = await Order.findByIdAndUpdate(
             req.params.id,
             {status},
-
             {new:true}
-            
-
-
         );
 
         if(!order) return res.status(404).json({message: "order not found"});
@@ -61,8 +53,6 @@ exports.updateOrder = async (req, res) => {
     }
 
 };
-
-
 exports.deleteOrder = async (req, res) => {
     try {
         const order = await Order.findByIdAndDelete(req.params.id);
